b72f5e4dfe568c526c12c71b65860be098ea4939
ackoppel <75754097+ackoppel@users.noreply.github.com>
2023-01-11 10:44:43 +0200

    Day 13 PT I

--- 13/index.js
+++ 13/index.js
@@ -8,11 +8,7 @@ class Solver {
   getSumOfIndicesOfOrderedPairs = () =>
     this.parseInput().reduce((acc, current, index) => {
       const [first, second] = current.split('\n').map(this.parseRow);
-      // console.log('\n\n\n ____ PAIR ____');
-      // console.log('First -> ', first);
-      // console.log('Second -> ', second);
-      const { ordered, itemFound } = this.pairIsOrdered(first, second);
-      // console.log('___ ORDERED --> ', ordered);
+      const { ordered } = this.pairIsOrdered(first, second);
       if (ordered) {
         acc += index + 1;
       }
@@ -20,28 +16,9 @@ class Solver {
     }, 0);
 
   pairIsOrdered = (left, right) => {
-    /** @todo -> Case when 1 is integer and need to be converted to an array */
-    // if (Array.isArray(left) && Number.isInteger(right)) {
-    //   console.log('\n\n\n');
-    //   console.log('Left -> ', left);
-    //   console.log('Right -> ', right);
-    //   return !!left.length ? left[0] <= right : true;
-    // }
-    // if (Number.isInteger(left) && Array.isArray(right)) {
-    //   console.log('\n\n\n');
-    //   console.log('Left -> ', left);
-    //   console.log('Right -> ', right);
-    //   return !!right.length ? left <= right[0] : false;
-    // }
-    if (left.length === 0 && right.length === 0) {
-      console.log('\n\n\n');
-      console.log('Left -> ', left);
-      console.log('Right -> ', right);
-    }
-
-    /** -- -- */
     const result = {
       ordered: false,
+      /** Needed to determine if recursive call found a comparison for decision */
       itemFound: false,
     };
 
@@ -50,24 +27,13 @@ class Solver {
       const elementFromRight = right[i];
 
       /** Right ends first */
-      if (!elementFromRight) {
+      if (elementFromRight === undefined) {
         result.itemFound = true;
         return result;
-        // return false;
       }
 
       /** One of the elements is an array, recursive call */
       if (Array.isArray(elementFromLeft) || Array.isArray(elementFromRight)) {
-        // console.log('\n\n\n');
-        // console.log(
-        //   'Left (getElementRepresentation)-> ',
-        //   this.getElementRepresentation(elementFromLeft)
-        // );
-        // console.log(
-        //   'Right (getElementRepresentation) -> ',
-        //   this.getElementRepresentation(elementFromRight)
-        // );
-
         const { itemFound, ordered } = this.pairIsOrdered(
           this.getElementRepresentation(elementFromLeft),
           this.getElementRepresentation(elementFromRight)
@@ -78,6 +44,7 @@ class Solver {
           return result;
         }
       } else if (elementFromLeft !== elementFromRight) {
+        /** First elements that can be compared */
         result.ordered = elementFromLeft < elementFromRight;
         result.itemFound = true;
         return result;
@@ -85,90 +52,11 @@ class Solver {
     }
 
     /** Left ends first OR no item to compare was yet found */
-    // return true;
-
     const found = left.length < right.length;
-    console.log('Left is shorter --> ', found);
     result.ordered = found;
     result.itemFound = found;
     return result;
   };
-
-  // pairIsOrdered = (left, right) => {
-  //   /** @todo -> Case when 1 is integer and need to be converted to an array */
-  //   if (Array.isArray(left) && Number.isInteger(right)) {
-  //     console.log('\n\n\n');
-  //     console.log('Left -> ', left);
-  //     console.log('Right -> ', right);
-  //     return !!left.length ? left[0] <= right : true;
-  //   }
-  //   if (Number.isInteger(left) && Array.isArray(right)) {
-  //     console.log('\n\n\n');
-  //     console.log('Left -> ', left);
-  //     console.log('Right -> ', right);
-  //     return !!right.length ? left <= right[0] : false;
-  //   }
-  //   /** -- -- */
-  //
-  //   for (let i = 0; i < left.length; i++) {
-  //     const elementFromLeft = left[i];
-  //     const elementFromRight = right[i];
-  //
-  //     /** Right ends first */
-  //     if (!elementFromRight) {
-  //       return false;
-  //     }
-  //
-  //     /** One of the elements is an array, recursive call */
-  //     if (Array.isArray(elementFromLeft) || Array.isArray(elementFromRight)) {
-  //       const ordered = this.pairIsOrdered(elementFromLeft, elementFromRight);
-  //       if (!ordered) {
-  //         return false;
-  //       }
-  //     } else if (elementFromLeft > elementFromRight) {
-  //       return false;
-  //     }
-  //   }
-  //
-  //   /** Left ends first and is ordered compared to right */
-  //   return true;
-  // };
-
-  /** @notice V1 */
-  // compareLevel = (left, right) => {
-  //   for (let i = 0; i < left.length; i++) {
-  //     /** Right ends first */
-  //     if (!right[i]) {
-  //       console.log('\n\n **** RIGHT ENDS FIRST **** ');
-  //       return false;
-  //     }
-  //
-  //     const elementFromLeft = this.getElementRepresentation(
-  //       left[i],
-  //       Array.isArray(right[i])
-  //     );
-  //     console.log('\n\n');
-  //     console.log('Element from left -> ', elementFromLeft);
-  //     const elementFromRight = this.getElementRepresentation(
-  //       right[i],
-  //       Array.isArray(elementFromLeft)
-  //     );
-  //     console.log('Element from right -> ', elementFromRight);
-  //
-  //     // One of the elements could be an array, recursive call
-  //     if (Array.isArray(elementFromLeft) && Array.isArray(elementFromRight)) {
-  //       const ordered = this.compareLevel(elementFromLeft, elementFromRight);
-  //       if (!ordered) {
-  //         return false;
-  //       }
-  //     } else if (elementFromLeft > elementFromRight) {
-  //       return false;
-  //     }
-  //   }
-  //
-  //   /** Left ends first and is ordered compared to right */
-  //   return true;
-  // };
 
   getElementRepresentation = (element) =>
     !Array.isArray(element) ? [element] : element;
