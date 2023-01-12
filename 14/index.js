d774eabebb1251f643442ef16544835cceb94252
ackoppel <75754097+ackoppel@users.noreply.github.com>
2023-01-12 23:40:24 +0200

    Day 14 PT II

--- 14/index.js
+++ 14/index.js
@@ -46,9 +46,9 @@ class Solver {
       }
     );
 
-  generateMap = () => {
+  generateMap = ({ withFloor }) => {
     const { paths, xMin, xMax, yMin, yMax } = this.parseInput();
-    const field = this.generateField(yMin, yMax, xMin, xMax);
+    const field = this.generateField({ yMin, yMax, xMin, xMax, withFloor });
     paths.forEach((pathConnections) => {
       for (const [connectionIndex, [x, y]] of Object.entries(pathConnections)) {
         const from = pathConnections[connectionIndex - 1];
@@ -81,20 +81,26 @@ class Solver {
     return numbers;
   };
 
-  generateField = (yMin, yMax, xMin, xMax) => {
+  generateField = ({ yMin, yMax, xMin, xMax, withFloor }) => {
+    const yLast = withFloor ? yMax + 2 : yMax;
     const xMapping = {};
     for (let i = xMin; i <= xMax; i++) {
       xMapping[i] = this.FieldSymbol.Empty;
     }
     const field = {};
-    for (let i = yMin; i <= yMax; i++) {
-      field[i] = { ...xMapping };
+    for (let i = yMin; i <= yLast; i++) {
+      if (withFloor && i === yLast) {
+        /** Add "Rock" symbol instead of object for easy access */
+        field[i] = this.FieldSymbol.Rock;
+      } else {
+        field[i] = { ...xMapping };
+      }
     }
     return field;
   };
 
-  findRestingUnitsOfSand = () => {
-    const map = this.generateMap();
+  fillMapWithSand = ({ withFloor }) => {
+    const map = this.generateMap({ withFloor });
     let filled = false;
     let count = 0;
     while (!filled) {
@@ -102,33 +108,62 @@ class Solver {
       if (resting) {
         count += 1;
       } else {
+        if (withFloor) {
+          count += 1;
+        }
         filled = true;
       }
     }
+    this.printMap(map);
     return count;
+  };
+
+  /** @param map {Record<number, Record<number, string>>} */
+  printMap = (map) => {
+    Object.entries(map).forEach(([key, row]) => {
+      console.log(`#${key} : `, Object.values(row).join(''));
+    });
   };
 
   generateSand = (map) => {
     let currentPosition = [...this.SandSource];
-    let resting = false;
-    while (!resting) {
+    let filled = false;
+    while (!filled) {
       const [x, y] = currentPosition;
-      /** Next step under current position is empty */
-      if (map[y + 1]?.[x] === this.FieldSymbol.Empty) {
+      const under = map[y + 1];
+
+      /**
+       * -- PT II --
+       * Filled if sand reaches top
+       * */
+      const filledBelowGenerator =
+        y === this.SandSource[1] &&
+        [under[x], under[x + 1], under[x - 1]].every(
+          (v) => v === this.FieldSymbol.Sand
+        );
+      const aboveExtendedFloor = under === this.FieldSymbol.Rock;
+
+      if (aboveExtendedFloor || filledBelowGenerator) {
+        map[y][x] = this.FieldSymbol.Sand;
+        return !!aboveExtendedFloor;
+      }
+
+      /** Falling down, account for unmapped width */
+      if (under?.[x] === this.FieldSymbol.Empty || (!!under && !under[x])) {
         currentPosition = [x, y + 1];
         continue;
       }
 
-      /** Left */
-      const left = map[y + 1]?.[x - 1];
-      if (left === this.FieldSymbol.Empty) {
+      /** Left, account for unmapped width  */
+      const left = under?.[x - 1];
+      if (left === this.FieldSymbol.Empty || (!!under && !left)) {
         currentPosition = [x - 1, y + 1];
         continue;
       }
 
-      /** Right */
-      const right = map[y + 1]?.[x + 1];
-      if (right === this.FieldSymbol.Empty) {
+      /** Right, account for unmapped width  */
+      const right = under?.[x + 1];
+      if (right === this.FieldSymbol.Empty || (!!under && !right)) {
         currentPosition = [x + 1, y + 1];
         continue;
       }
@@ -138,7 +173,7 @@ class Solver {
       }
 
       map[y][x] = this.FieldSymbol.Sand;
-      resting = true;
+      filled = true;
     }
     return true;
   };
@@ -147,6 +182,11 @@ class Solver {
 const solver = new Solver();
 
 console.log(
-  'Units of sand that come to rest -> ',
-  solver.findRestingUnitsOfSand()
+  'Units of sand that come to rest on structure -> ',
+  solver.fillMapWithSand({})
+);
+
+console.log(
+  'Units of sand that come to rest on floor -> ',
+  solver.fillMapWithSand({ withFloor: true })
 );
